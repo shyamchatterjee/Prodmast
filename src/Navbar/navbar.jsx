3cf582f50116d9b36842782200674844bbbe03ce
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-07-16 00:43:25 +0530

    project update

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -2,13 +2,14 @@ import { useContext, useState } from "react";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
 import { Context } from "../../Context/context";
+import { FaBars } from "react-icons/fa6";
 let Navbar = ()=>{
    
    let {bool,setBool} = useContext(Context)
     return  <div className=" nav  w-full  mt-2  rounded-full bg-white flex md:justify-around items-center md:flex-row justify-between p-4   ">
           <div className="flex items-center">
-                <img src="https://tse2.mm.bing.net/th/id/OIP.CTZVRe2QsSp0H7iUwdTpwQHaEv?r=0&pid=ImgDet&w=474&h=303&rs=1&o=7&rm=3" className="h-8 w-8 md:w-13 md:h-10" alt="" />
-                <p className="  text-1xl   md: text-2xl">Prodmast</p>
+                <img src="https://cdn.vectorstock.com/i/500p/45/61/lion-head-logo-esports-line-art-vector-52294561.jpg" className="h-8 w-10 md:w-13 md:h-10" alt="" />
+                <p style={{fontSize:"20px"}}>Prodmast</p>
           </div>
           <div className=" hidden md:flex items-center justify-around  gap-3 text-2xl">
             <p>Home</p>
@@ -24,8 +25,8 @@ let Navbar = ()=>{
             <p className="peras  ">Contect</p>
             <button className=" bg-green-950 rounded-full w-26 p-2  text-white cursor-pointer">Sign Up</button>
           </div>
-          <button className="  hidden md:block bg-green-950 rounded-full w-25 p-3  text-white cursor-pointer">Sign Up</button>
-           <FaBarsStaggered onClick={()=>{setBool(true)}}  className=" md:hidden  text-3xl cursor-pointer text-black"/> 
+          <button className=" hidden md:block bg-green-950 rounded-full w-30  p-2  text-white cursor-pointer">Sign Up</button>
+           <FaBars onClick={()=>{setBool(true)}}  className=" md:hidden  text-3xl cursor-pointer text-black"/> 
         
           
     </div>
