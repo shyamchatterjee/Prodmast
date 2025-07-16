b6897b173c5f95aaae4b3446e35d8cd6fa3ad1cd
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-07-16 15:34:34 +0530

    update-button

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -3,33 +3,49 @@ import { FaBarsStaggered } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
 import { Context } from "../../Context/context";
 import { FaBars } from "react-icons/fa6";
-let Navbar = ()=>{
-   
-   let {bool,setBool} = useContext(Context)
-    return  <div className=" nav  w-full  mt-2   rounded-full bg-white flex md:justify-around items-center md:flex-row justify-between p-4   ">
-          <div className="flex items-center">
-                <img src="https://cdn.vectorstock.com/i/500p/45/61/lion-head-logo-esports-line-art-vector-52294561.jpg" className="h-8 w-10 md:w-13 md:h-10" alt="" />
-                <p style={{fontSize:"20px"}}>Prodmast</p>
-          </div>
-          <div className=" hidden md:flex items-center justify-around  gap-3 text-2xl">
-            <p>Home</p>
-            <p>About</p>
-            <p>Service</p>
-            <p>Contect</p>
-          </div>
-           <div className={bool==false?"hidden":" block bar md:hidden"}>
-            <IoMdClose  className="ml-8  text-2xl cursor-pointer mt-10  "   onClick={()=>{setBool(false)}}/>
-            <p className="peras  ">Home</p>
-            <p className="peras ">About</p>
-            <p className="peras ">Service</p>
-            <p className="peras  ">Contect</p>
-            <button className=" bg-green-950 rounded-full w-26 p-2  text-white cursor-pointer">Sign Up</button>
-          </div>
-          <button className=" hidden md:block bg-green-950 rounded-full w-30  p-2  text-white cursor-pointer">Sign Up</button>
-           <FaBars onClick={()=>{setBool(true)}}  className=" md:hidden  text-3xl cursor-pointer text-black"/> 
-        
-          
+let Navbar = () => {
+  let { bool, setBool } = useContext(Context);
+  return (
+    <div className=" nav  w-full  mt-2   rounded-full bg-white flex md:justify-around items-center md:flex-row justify-between p-4   ">
+      <div className="flex items-center">
+        <img
+          src="https://cdn.vectorstock.com/i/500p/45/61/lion-head-logo-esports-line-art-vector-52294561.jpg"
+          className="h-8 w-10 md:w-13 md:h-10"
+          alt=""
+        />
+        <p style={{ fontSize: "20px" }}>Prodmast</p>
+      </div>
+      <div className=" hidden md:flex items-center justify-around  gap-3 text-2xl">
+        <p>Home</p>
+        <p>About</p>
+        <p>Service</p>
+        <p>Contect</p>
+      </div>
+      <div className={bool == false ? "hidden" : " block bar md:hidden"}>
+        <IoMdClose
+          className="ml-8  text-2xl cursor-pointer mt-10  "
+          onClick={() => {
+            setBool(false);
+          }}
+        />
+        <p className="peras  ">Home</p>
+        <p className="peras ">About</p>
+        <p className="peras ">Service</p>
+        <p className="peras  ">Contect</p>
+        <button className="button">
+          Sign Up
+        </button>
+      </div>
+      <button className=" hidden md:block bg-green-950 rounded-full w-30  p-2  text-white cursor-pointer">
+        Sign Up
+      </button>
+      <FaBars
+        onClick={() => {
+          setBool(true);
+        }}
+        className=" md:hidden  text-3xl cursor-pointer text-black"
+      />
     </div>
-  
-}
-export default Navbar
+  );
+};
+export default Navbar;
